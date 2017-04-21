9104b0910daf698521992c5d690e7b995dff0bda
zeashon <zeashon@gmail.com>
2017-04-21 16:06:17 +0800

    add login(include xml png).

--- app/src/main/java/jne/com/post/MainActivity.java
+++ app/src/main/java/jne/com/post/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_pre);
+        setContentView(R.layout.activity_main);
 
         ordersDao = new OrderDao(this);
         if (!ordersDao.isDataExist()) {
