b7e8b7a7a22ac654568bbfbbd50e81e86be8199a
zeashon <zeashon@gmail.com>
2017-04-28 22:02:25 +0800

    bug fixed: add file zxing.jar turn splash to full screen.

--- app/src/main/java/jne/com/post/SplashActivity.java
+++ app/src/main/java/jne/com/post/SplashActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import jne.com.R;
 
@@ -13,6 +15,11 @@ public class SplashActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //无title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //全屏
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams. FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
