93a4b55750d21187e8c84809e8c00ec59bc4bd76
zeashon <zeashon@gmail.com>
2017-05-04 01:11:36 +0800

    optimize post-item ui.

--- app/src/main/java/jne/com/post/OrderListAdapter.java
+++ app/src/main/java/jne/com/post/OrderListAdapter.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import jne.com.R;
 
-public class OrderListAdapter extends BaseAdapter{
+public class OrderListAdapter extends BaseAdapter {
     private String TAG = "OrderListAdapter";
     private Context context;
     private List<Order> orderList;
@@ -39,21 +39,17 @@ public class OrderListAdapter extends BaseAdapter{
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
         Order order = orderList.get(position);
-        if (order == null){
+        if (order == null) {
             return null;
         }
 
         ViewHolder holder = null;
-        if (view != null){
+        if (view != null) {
             holder = (ViewHolder) view.getTag();
-        }else {
+        } else {
             view = LayoutInflater.from(context).inflate(R.layout.show_post_item, null);
 
             holder = new ViewHolder();
-//            holder.dateIdTextView = (TextView) view.findViewById(R.id.dateIdTextView);
-//            holder.dateCustomTextView = (TextView) view.findViewById(R.id.dateCustomTextView);
-//            holder.dateOrderPriceTextView = (TextView) view.findViewById(R.id.dateOrderPriceTextView);
-//            holder.dateCountoryTextView = (TextView) view.findViewById(R.id.dateCountoryTextView);
             holder.user_name = (TextView) view.findViewById(R.id.user_name);
             holder.postItem_time = (TextView) view.findViewById(R.id.postItem_time);
             holder.finishTime = (TextView) view.findViewById(R.id.finishTime);
@@ -67,25 +63,20 @@ public class OrderListAdapter extends BaseAdapter{
             view.setTag(holder);
         }
 
-//        holder.dateIdTextView.setText(order.id + "");
-//        holder.dateCustomTextView.setText(order.customName);
-//        holder.dateOrderPriceTextView.setText(order.phoneNum + "");
-//        holder.dateCountoryTextView.setText(order.country);
-
         holder.user_name.setText(order.customName);
-        holder.postItem_time.setText(order.id +"发布");
+        holder.postItem_time.setText(order.id + "发布");
         holder.finishTime.setText(order.finishTime);
         holder.finishPlace.setText(order.finishPlace);
         holder.finishStation.setText(order.country);
-        holder.remuneration.setText("诱惑："+order.remuneration);
-        holder.phoneNumText.setText("tel:"+order.phoneNum + "");
+        holder.remuneration.setText("诱惑：" + order.remuneration);
+        holder.phoneNumText.setText("tel:" + order.phoneNum + "");
         holder.postDetail.setText(order.postDetail);
-        if(order.type == 0){
-            holder.addCollection.setText("点击收藏");
-        }else if(order.type == 2){
-            holder.addCollection.setText("取消收藏");
-        }else if(order.type == 1){
-            holder.addCollection.setText("这是你的帖子");
+        if (order.type == 0) {
+            holder.addCollection.setBackgroundResource(R.drawable.icon_like_empty);
+        } else if (order.type == 2) {
+            holder.addCollection.setBackgroundResource(R.drawable.icon_like_filled);
+        } else if (order.type == 1) {
+            holder.addCollection.setBackgroundResource(R.drawable.icon_like_filled);
             holder.addCollection.setClickable(false);
         }
         final String id = order.id;
@@ -93,22 +84,23 @@ public class OrderListAdapter extends BaseAdapter{
         final OrderDao ordersDao;
         final ViewHolder oholder = holder;
         ordersDao = new OrderDao(this.context);
-        if (! ordersDao.isDataExist()){
+        if (!ordersDao.isDataExist()) {
             ordersDao.initTable();
         }
         holder.addCollection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e(TAG,"点击"+ id +"的收藏了");
-                ordersDao.addPost(id,tag);
-                if(2-tag == 2){
-                    oholder.addCollection.setText("点击收藏");
-                }else if(2-tag == 2){
-                    oholder.addCollection.setText("取消收藏");
+                Log.e(TAG, "点击" + id);
+                ordersDao.addPost(id, tag);
+                if (2 - tag == 2) {
+                    Log.e(TAG, "取消收藏了帖子" + id + "。");
+                    oholder.addCollection.setBackgroundResource(R.drawable.icon_like_empty);
+                } else if (2 - tag == 2) {
+                    Log.e(TAG, "收藏了帖子" + id + "。");
+                    oholder.addCollection.setBackgroundResource(R.drawable.icon_like_filled);
                 }
             }
         });
-
         return view;
     }
 
@@ -117,11 +109,7 @@ public class OrderListAdapter extends BaseAdapter{
         return false;
     }
 
-    public static class ViewHolder{
-//        public TextView dateIdTextView;
-//        public TextView dateCustomTextView;
-//        public TextView dateOrderPriceTextView;
-//        public TextView dateCountoryTextView;
+    public static class ViewHolder {
         public TextView user_name;
         public TextView postItem_time;
         public TextView finishTime;
