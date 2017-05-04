7e7f4caa160c135f41afdad5e16c1401c14db387
zeashon <zeashon@gmail.com>
2017-05-04 20:51:24 +0800

    1. optimize post-item ui: add call & text Button; 2. add call & text function;

--- app/src/main/java/jne/com/post/OrderListAdapter.java
+++ app/src/main/java/jne/com/post/OrderListAdapter.java
@@ -1,11 +1,18 @@
 package jne.com.post;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.*;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -48,7 +55,6 @@ public class OrderListAdapter extends BaseAdapter {
             holder = (ViewHolder) view.getTag();
         } else {
             view = LayoutInflater.from(context).inflate(R.layout.show_post_item, null);
-
             holder = new ViewHolder();
             holder.user_name = (TextView) view.findViewById(R.id.user_name);
             holder.postItem_time = (TextView) view.findViewById(R.id.postItem_time);
@@ -59,17 +65,23 @@ public class OrderListAdapter extends BaseAdapter {
             holder.phoneNumText = (TextView) view.findViewById(R.id.phoneNumText);
             holder.postDetail = (TextView) view.findViewById(R.id.postDetail);
             holder.addCollection = (Button) view.findViewById(R.id.addCollection);
-
+            holder.callMe = (ImageButton) view.findViewById(R.id.call_me);
+            holder.textMe = (ImageButton) view.findViewById(R.id.text_me);
             view.setTag(holder);
         }
 
         holder.user_name.setText(order.customName);
-        holder.postItem_time.setText(order.id + "发布");
+        String year, month, day;
+        year = order.id.substring(0, 4);
+        month = order.id.substring(4, 6);
+        day = order.id.substring(6, 8);
+        Log.e(TAG, year + ":" + month + ":" + day);
+        holder.postItem_time.setText(year + "年" + (Integer.parseInt(month) - 1) + "月" + day + "日");
         holder.finishTime.setText(order.finishTime);
         holder.finishPlace.setText(order.finishPlace);
         holder.finishStation.setText(order.country);
-        holder.remuneration.setText("诱惑：" + order.remuneration);
-        holder.phoneNumText.setText("tel:" + order.phoneNum + "");
+        holder.remuneration.setText("价格：" + order.remuneration);
+        holder.phoneNumText.setText("联系方式:" + order.phoneNum + "");
         holder.postDetail.setText(order.postDetail);
         if (order.type == 0) {
             holder.addCollection.setBackgroundResource(R.drawable.icon_like_empty);
@@ -87,7 +99,7 @@ public class OrderListAdapter extends BaseAdapter {
         if (!ordersDao.isDataExist()) {
             ordersDao.initTable();
         }
-        holder.addCollection.setOnClickListener(new View.OnClickListener() {
+        oholder.addCollection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.e(TAG, "点击" + id);
@@ -101,7 +113,82 @@ public class OrderListAdapter extends BaseAdapter {
                 }
             }
         });
+        String str[] = oholder.phoneNumText.getText().toString().split(":");
+        final String PhoneNum = str[1];
+        oholder.textMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(PhoneNum)) {
+                    sendSmsWithNumber(context, PhoneNum, "您好，我在易出行上看到你的帖子。请问");
+                }
+                Toast.makeText(context, "即将跳转短信编辑页", Toast.LENGTH_SHORT).show();
+            }
+        });
+        oholder.callMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //    通过AlertDialog.Builder这个类来实例化我们的一个AlertDialog的对象
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                //    设置Title的图标
+                builder.setIcon(R.drawable.logo);
+                //    设置Title的内容
+                builder.setTitle("拨打电话确认");
+                //    设置Content来显示一个信息
+                builder.setMessage("确定要拨打电话给" + PhoneNum + "吗？");
+                //    设置一个PositiveButton
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (!TextUtils.isEmpty(PhoneNum)) {
+                            callWithNumber(context,PhoneNum);
+                        }
+                        Toast.makeText(context, "即将跳转电话拨打界面", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                //    设置一个NegativeButton
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "已取消打电话" + which, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                //    显示出该对话框
+                builder.show();
+            }
+        });
         return view;
+    }
+
+    /**
+     * 调用系统界面，给指定的号码发送短信
+     *
+     * @param context
+     * @param number
+     */
+    public void sendSmsWithNumber(Context context, String number, String body) {
+        Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+        sendIntent.setData(Uri.parse("smsto:" + number));
+        sendIntent.putExtra("sms_body", body);
+        context.startActivity(sendIntent);
+    }
+    /**
+     * 调用系统界面，给指定的号码打电话
+     *
+     * @param context
+     * @param number
+     */
+    public void callWithNumber(Context context, String number) {
+//    使用Uri.parse(String a)创建Uri。
+        Uri uri = Uri.parse("tel:" + number);
+//    创建打电话的意图。
+        Intent callIntent = new Intent(Intent.ACTION_CALL, uri);
+//    启动系统打电话页面。
+        try {
+            context.startActivity(callIntent);
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(context, "没有打电话的权限", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -119,5 +206,9 @@ public class OrderListAdapter extends BaseAdapter {
         public TextView phoneNumText;
         public TextView postDetail;
         public Button addCollection;
+        public ImageButton textMe;
+        public ImageButton callMe;
     }
+
+
 }
